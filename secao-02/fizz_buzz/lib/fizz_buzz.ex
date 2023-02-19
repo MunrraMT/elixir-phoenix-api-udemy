347f707cef46e9f5e7e645143b2577d74710ab35
André Rodrigues <andre.rodrigues@agencian1.com.br>
2023-02-19 00:35:25 -0400

    18. Lendo o arquivo usando pattern matching

--- secao-02/fizz_buzz/lib/fizz_buzz.ex
+++ secao-02/fizz_buzz/lib/fizz_buzz.ex
@@ -1,18 +1,10 @@
 defmodule FizzBuzz do
-  @moduledoc """
-  Documentation for `FizzBuzz`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FizzBuzz.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def build(file_name) do
+    file_name
+    |> File.read()
+    |> handle_file_read()
   end
+
+  defp handle_file_read({:ok, result}), do: result
+  defp handle_file_read({:error, reason}), do: reason
 end
