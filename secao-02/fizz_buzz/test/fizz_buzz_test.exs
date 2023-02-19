2b9b7b6ea752dd0b443555c4cdda4d768f75efbf
André Rodrigues <andre.rodrigues@agencian1.com.br>
2023-02-19 01:31:43 -0400

    23. Testando nosso Projeto

--- secao-02/fizz_buzz/test/fizz_buzz_test.exs
+++ secao-02/fizz_buzz/test/fizz_buzz_test.exs
@@ -1,8 +1,15 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
-  doctest FizzBuzz
 
-  test "greets the world" do
-    assert FizzBuzz.hello() == :world
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+      expected_response = {:ok, [1, 2, :Fizz, 4, :Buzz, :Buzz, :FizzBuzz, :Buzz]}
+      assert FizzBuzz.build('numbers.txt') == expected_response
+    end
+
+    test "when a invalid file is provided, returns the error message" do
+      expected_response = {:error, "Error reading the file: enoent"}
+      assert FizzBuzz.build('numberss.txt') == expected_response
+    end
   end
 end
