0cac4b42b57b8a1bdfe545d39e77bc858fdef70f
André Rodrigues <andre.rodrigues@agencian1.com.br>
2023-02-23 22:58:10 -0400

    44. Terminando os testes

--- secao-03/ex_mon/test/ex_mon_test.exs
+++ secao-03/ex_mon/test/ex_mon_test.exs
@@ -3,7 +3,7 @@ defmodule ExMonTest do
 
   import ExUnit.CaptureIO
 
-  alias ExMon.{Player}
+  alias ExMon.{Game, Player}
 
   describe "create_player/4" do
     test "returns a player" do
@@ -24,10 +24,46 @@ defmodule ExMonTest do
       name: "André"
     }
 
-    messages = capture_io(fn -> assert ExMon.start_game(player) == :ok end)
+    messages =
+      capture_io(fn ->
+        assert ExMon.start_game(player) == :ok
+      end)
 
     assert messages =~ "The game is started!"
     assert messages =~ "status: :started"
     assert messages =~ "turn: :player"
+  end
+
+  describe "make_move/1" do
+    setup do
+      player = ExMon.create_player("André", :chute, :soco, :cura)
+
+      capture_io(fn ->
+        ExMon.start_game(player)
+      end)
+
+      :ok
+    end
+
+    test "when the move is valid do the move and the computer makes a move" do
+      messages =
+        capture_io(fn ->
+          ExMon.make_move(:chute)
+        end)
+
+      assert messages =~ "Player attacked the Computer dealing"
+      assert messages =~ "It's computer turn!"
+      assert messages =~ "It's player turn!"
+      assert messages =~ "status: :continue"
+    end
+
+    test "when the move is invalid, returns an error message" do
+      messages =
+        capture_io(fn ->
+          ExMon.make_move(:wrong)
+        end)
+
+      assert messages =~ "invalid move: wrong!"
+    end
   end
 end
