07f378107f58416e7c48628bc3927af7a836fc24
André Rodrigues <andre.rodrigues@agencian1.com.br>
2023-02-26 00:03:08 -0400

    54. Encriptando o password do Trainer.

--- secao-04/ex_mon/mix.exs
+++ secao-04/ex_mon/mix.exs
@@ -43,7 +43,8 @@ defmodule ExMon.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:argon2_elixir, "~> 3.0"}
     ]
   end
 
