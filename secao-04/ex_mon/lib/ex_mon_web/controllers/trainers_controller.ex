857d8ad46bf495e402a12df5344605fa24abbb2f
André Rodrigues <andre.rodrigues@agencian1.com.br>
2023-03-01 08:23:59 -0400

    60. Criando a rota de delete

--- secao-04/ex_mon/lib/ex_mon_web/controllers/trainers_controller.ex
+++ secao-04/ex_mon/lib/ex_mon_web/controllers/trainers_controller.ex
@@ -6,14 +6,26 @@ defmodule ExMonWeb.TrainersController do
   def create(conn, params) do
     params
     |> ExMon.create_trainer()
-    |> handle_response(conn)
+    |> handle_response(conn, :create)
   end
 
-  defp handle_response({:ok, trainer}, conn) do
+  def delete(conn, %{"id" => id}) do
+    id
+    |> ExMon.delete_trainer()
+    |> handle_response(conn, :delete)
+  end
+
+  defp handle_response({:ok, trainer}, conn, :create) do
     conn
     |> put_status(:created)
     |> render("create.json", trainer: trainer)
   end
 
-  defp handle_response({:error, __trainer} = error, __conn), do: error
+  defp handle_response({:ok, __trainer}, conn, :delete) do
+    conn
+    |> put_status(:no_content)
+    |> text("")
+  end
+
+  defp handle_response({:error, __trainer} = error, __conn, __method), do: error
 end
