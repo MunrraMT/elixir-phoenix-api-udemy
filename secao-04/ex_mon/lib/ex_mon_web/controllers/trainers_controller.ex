3b72f4eff84e5498161faf1f293c94a07b9a156f
André Rodrigues <andre.rodrigues@agencian1.com.br>
2023-03-01 09:57:32 -0400

    63. Refatorando nosso Trainers Controller

--- secao-04/ex_mon/lib/ex_mon_web/controllers/trainers_controller.ex
+++ secao-04/ex_mon/lib/ex_mon_web/controllers/trainers_controller.ex
@@ -6,26 +6,34 @@ defmodule ExMonWeb.TrainersController do
   def create(conn, params) do
     params
     |> ExMon.create_trainer()
-    |> handle_response(conn, :create)
+    |> handle_response(conn, "create.json", :created)
   end
 
   def delete(conn, %{"id" => id}) do
     id
     |> ExMon.delete_trainer()
-    |> handle_response(conn, :delete)
+    |> handle_delete(conn)
   end
 
-  defp handle_response({:ok, trainer}, conn, :create) do
-    conn
-    |> put_status(:created)
-    |> render("create.json", trainer: trainer)
+  def show(conn, %{"id" => id}) do
+    id
+    |> ExMon.fetch_trainer()
+    |> handle_response(conn, "show.json", :ok)
   end
 
-  defp handle_response({:ok, __trainer}, conn, :delete) do
+  defp handle_delete({:error, _reason} = error, _conn), do: error
+
+  defp handle_delete({:ok, _trainer}, conn) do
     conn
     |> put_status(:no_content)
     |> text("")
   end
 
-  defp handle_response({:error, __reason} = error, __conn, __method), do: error
+  defp handle_response({:error, _reason} = error, _conn, _view, _status_code), do: error
+
+  defp handle_response({:ok, trainer}, conn, view, status_code) do
+    conn
+    |> put_status(status_code)
+    |> render(view, trainer: trainer)
+  end
 end
--- secao-04/ex_mon/lib/ex_mon_web/views/trainers_view.ex
+++ secao-04/ex_mon/lib/ex_mon_web/views/trainers_view.ex
@@ -13,4 +13,12 @@ defmodule ExMonWeb.TrainersView do
       }
     }
   end
+
+  def render("show.json", %{trainer: %Trainer{id: id, name: name, inserted_at: inserted_at}}) do
+    %{
+      id: id,
+      name: name,
+      inserted_at: inserted_at
+    }
+  end
 end
