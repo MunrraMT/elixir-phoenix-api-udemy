eef0409b385b2d7c150fd80ee830fc3c94ef9d4c
André Rodrigues <andre.rodrigues@agencian1.com.br>
2023-03-01 08:36:14 -0400

    62. Criando o GET trainer

---
+++ secao-04/ex_mon/lib/ex_mon/trainer/get.ex
@@ -0,0 +1,20 @@
+defmodule ExMon.Trainer.Get do
+  alias ExMon.{Repo, Trainer}
+  alias Ecto.UUID
+
+  def call(uuid) do
+    case UUID.cast(uuid) do
+      :error -> {:error, "Invalid ID format!"}
+      {:ok, uuid} -> get(uuid)
+    end
+  end
+
+  defp get(uuid) do
+    case fetch_trainer(uuid) do
+      nil -> {:error, "Trainer not found!"}
+      trainer -> {:ok, trainer}
+    end
+  end
+
+  defp fetch_trainer(uuid), do: Repo.get(Trainer, uuid)
+end
