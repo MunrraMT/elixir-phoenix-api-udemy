a9e3a1de4e799b750b940dc362c3377843631a20
André Rodrigues <andre.rodrigues@agencian1.com.br>
2023-03-01 22:00:35 -0400

    68. Criando nosso Client HTTP

--- secao-04/ex_mon/mix.exs
+++ secao-04/ex_mon/mix.exs
@@ -44,7 +44,9 @@ defmodule ExMon.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:argon2_elixir, "~> 3.0"}
+      {:argon2_elixir, "~> 3.0"},
+      {:tesla, "~> 1.5"},
+      {:hackney, "~> 1.18"}
     ]
   end
 
