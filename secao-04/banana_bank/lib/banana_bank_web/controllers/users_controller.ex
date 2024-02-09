f809c5d16a1ef49c5e84ea3649055dcde03bcf47
MunrraMT <dev.armsantos@gmail.com>
2024-02-08 23:54:10 -0400

    55. Criando usuário através da API

---
+++ secao-04/banana_bank/lib/banana_bank_web/controllers/users_controller.ex
@@ -0,0 +1,22 @@
+defmodule BananaBankWeb.UsersController do
+  use BananaBankWeb, :controller
+  alias BananaBank.Users.Create
+
+  def create(conn, params) do
+    params
+    |> Create.call()
+    |> handle_response(conn)
+  end
+
+  defp handle_response({:ok, user}, conn) do
+    conn
+    |> put_status(:created)
+    |> render("user.json", user: user)
+  end
+
+  defp handle_response({:error, _changeset} = error, conn) do
+    conn
+    |> put_status(:bad_request)
+    |> render("error.json", error: error)
+  end
+end
