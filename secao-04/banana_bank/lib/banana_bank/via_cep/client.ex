ca91a12b95c8ceb304a264ff0098363e5c133d7b
MunrraMT <dev.armsantos@gmail.com>
2024-02-12 19:12:20 -0400

    69. Criando nosso client

---
+++ secao-04/banana_bank/lib/banana_bank/via_cep/client.ex
@@ -0,0 +1,26 @@
+defmodule BananaBank.ViaCep.Client do
+  use Tesla
+
+  plug Tesla.Middleware.BaseUrl, "https://viacep.com.br/ws"
+  plug Tesla.Middleware.JSON
+
+  def call(cep) do
+    "/#{cep}/json/"
+    |> get()
+    |> handle_response()
+  end
+
+  defp handle_response({:error, _raison}), do: {:error, :internal_server_error}
+
+  defp handle_response({:ok, %Tesla.Env{status: 400}}) do
+    {:error, :bad_request}
+  end
+
+  defp handle_response({:ok, %Tesla.Env{status: 200, body: %{"erro" => true}}}) do
+    {:error, :not_found}
+  end
+
+  defp handle_response({:ok, %Tesla.Env{status: 200, body: body}}) do
+    {:ok, body}
+  end
+end
