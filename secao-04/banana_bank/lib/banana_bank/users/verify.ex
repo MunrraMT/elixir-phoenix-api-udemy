5f9791e43104cf4e0bc215f0c919aa6d6ca9c8ad
MunrraMT <dev.armsantos@gmail.com>
2024-02-15 09:19:42 -0400

    84. Verificando o password do User

---
+++ secao-04/banana_bank/lib/banana_bank/users/verify.ex
@@ -0,0 +1,21 @@
+defmodule BananaBank.Users.Verify do
+  alias BananaBank.Users.User
+  alias BananaBank.Users
+
+  def call(%{"user_id" => user_id, "password" => password}) do
+    case Users.get(user_id) do
+      {:ok, %User{password_hash: password_hash} = _user} ->
+        verify(password, password_hash)
+
+      error ->
+        error
+    end
+  end
+
+  defp verify(password, password_hash) do
+    case Argon2.verify_pass(password, password_hash) do
+      true -> {:ok, :valid_password}
+      false -> {:error, :unauthorized}
+    end
+  end
+end
